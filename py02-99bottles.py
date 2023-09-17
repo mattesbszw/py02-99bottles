4e66edecfdd2f834c210ba8380a577b0b8de16b5
mattesbszw <107214376+mattesbszw@users.noreply.github.com>
2023-09-17 12:54:34 +0200

    Initial commit

---
+++ py02-99bottles.py
@@ -0,0 +1,13 @@
+for i in (99,0,-1)
+    if i == 1
+        print ("1 bottle of beer on the wall, 1 bottle of beer.")
+        print ("Take one down and pass it around, no more bottles of beer to go")
+    elif i == 2
+        print (i," bottles of beer on the wall,", i, "bottles of beer.")
+        print ("Take one down and pass it around,", i-1, "bottle of beer on the wall.")
+    else
+        print (i," bottles of beer on the wall,", i, "bottles of beer.")
+        print ("Take one down and pass it around,", i-1, "bottles of beer on the wall.")
+
+    print ("No more bottles of beer on the wall, no more bottles of beer.")
+        print ("Go to the store and buy some more, 99 bottles of beer on the wall")
